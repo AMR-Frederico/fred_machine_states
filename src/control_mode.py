b8b3e00b540914debeb7887986767c171a3d0a5f
DinossauroBebado <guipsilva32@gmail.com>
2023-02-17 12:05:01 +0000

    first commit

---
+++ src/control_mode.py
@@ -0,0 +1,8 @@
+
+# change between manual control and autonomos mode 
+
+
+#flage mode -> # fred/states/controler/ -> controler 
+#                                       -> auto 
+
+
---
+++ src/goal_mode.py
@@ -0,0 +1,14 @@
+
+
+
+#machine states for moving the robot 
+
+
+#state 01 - > idle  -> stoped motors led yellow 
+# ----start moving  
+#state 02 -> with goal -> actived PID led blue 
+# --- moving---- 
+#state 03 -> achieved goal  ->  led green 
+# ---- stop 
+
+
